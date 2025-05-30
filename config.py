ae145f4c0c1cd6072afc82efe12a8a9a4e32d65f
Abdu_qahhor_2o1o <asunnataliyev10@gmail.com>
2025-05-30 09:45:03 +0500

    ozgardi

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN='7770700132:AAHyv8mMtj0jSPLD7pB8ec-YLJYHYI1-d1g'
-CHANNELS_URL = ['https://t.me/asilbekmirolimov', 
+BOT_TOKEN='8031092542:AAFC1UI7HeZH71EpgH21bu8DcZoLq0AAGvI'
+CHANNELS_URL = ['https://t.me/dddddddddwa', 
                 'https://t.me/Abduqahhor_is_history',
-                'https://t.me/trentmyuzik']
-CHANNELS_ID = ['-1001687130068', '-1002473956918', '-1002133045821']
+                'https://t.me/fyhgeryghre']
+CHANNELS_ID = ['-1002601721105', '-1002510418366', '-1002473956918']
